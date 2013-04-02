5fb9c29557a2d52a8c5d1c9df973faa9d0f9ae67
Paul Mandel <paul.mand3l@gmail.com>
2013-04-02 13:20:31 -0400

    Updating images in slider

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -25,21 +25,21 @@ jQuery(document).ready(function() {
 jQuery(document).ready(function() {
     $clientsHolder = $('ul.portfolio-img');
     $clientsClone = $clientsHolder.clone();
- 
+
     $('.filter-portfolio a').click(function(e) {
         e.preventDefault();
         $filterClass = $(this).attr('class');
- 
+
         $('.filter-portfolio a').attr('id', '');
         $(this).attr('id', 'active-imgs');
- 
+
         if($filterClass == 'all'){
             $filters = $clientsClone.find('li');
         }
         else {
             $filters = $clientsClone.find('li[data-type~='+ $filterClass +']');
         }
- 
+
         $clientsHolder.quicksand($filters, {duration: 700}, function() {
             $("a[rel^='prettyPhoto']").prettyPhoto({social_tools: false});
         });
